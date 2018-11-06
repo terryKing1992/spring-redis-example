f3e32560be855897a94d4e1433bd179a76d68b0a
terryKing1992 <terryllmay.gmail.com>
2018-11-06 21:51:53 +0800

    增加关于cluster模式的java访问

--- src/main/java/com/terrylmay/redis/example/RedisExampleApplication.java
+++ src/main/java/com/terrylmay/redis/example/RedisExampleApplication.java
@@ -1,15 +1,22 @@
 package com.terrylmay.redis.example;
 
+import com.terrylmay.redis.example.config.ClusterRedisConfig;
 import com.terrylmay.redis.example.config.StandaloneRedisConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication(scanBasePackages = {"com.terrylmay.redis.example"})
 public class RedisExampleApplication {
@@ -18,16 +25,31 @@ public class RedisExampleApplication {
         SpringApplication.run(RedisExampleApplication.class, args);
     }
 
-    @Autowired
+    @Autowired(required = false)
     StandaloneRedisConfig standaloneRedisConfig;
+
+    @Autowired(required = false)
+    ClusterRedisConfig clusterRedisConfig;
 
     @Autowired
     RedisConnectionFactory redisConnectionFactory;
 
     @Bean
-    @ConditionalOnBean(value = {StandaloneRedisConfig.class})
-    public RedisConnectionFactory standaloneRedisConnectionFactory() {
-        JedisConnectionFactory factory = new JedisConnectionFactory(new RedisStandaloneConfiguration(standaloneRedisConfig.getHost(), standaloneRedisConfig.getPort()));
+    public RedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory factory = null;
+        if (standaloneRedisConfig != null) {
+            factory = new JedisConnectionFactory(new RedisStandaloneConfiguration(standaloneRedisConfig.getHost(), standaloneRedisConfig.getPort()));
+            return factory;
+        }
+
+        if (clusterRedisConfig != null) {
+            JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+            RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(Arrays.asList(clusterRedisConfig.getNodes().split(",")));
+            redisClusterConfiguration.setMaxRedirects(clusterRedisConfig.getMaxRedirects());
+            redisClusterConfiguration.setPassword(clusterRedisConfig.getPassword());
+            factory = new JedisConnectionFactory(redisClusterConfiguration, jedisPoolConfig);
+        }
+
         return factory;
     }
 
--- src/main/java/com/terrylmay/redis/example/config/ClusterRedisConfig.java
+++ src/main/java/com/terrylmay/redis/example/config/ClusterRedisConfig.java
@@ -1,4 +1,41 @@
 package com.terrylmay.redis.example.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis.cluster")
+@ConditionalOnProperty(name = {"spring.redis.cluster.nodes"})
 public class ClusterRedisConfig {
+
+    private String nodes;
+
+    private String password;
+
+    private int maxRedirects;
+
+    public String getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(String nodes) {
+        this.nodes = nodes;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getMaxRedirects() {
+        return maxRedirects;
+    }
+
+    public void setMaxRedirects(int maxRedirects) {
+        this.maxRedirects = maxRedirects;
+    }
 }
