1d258df013a7663439be3292c05d5aeaa095ece9
liyuan <761686949@qq.com>
2019-05-30 20:47:52 +0800

    978

--- src/test/java/com/terrylmay/redis/example/RedisExampleApplicationTests.java
+++ src/test/java/com/terrylmay/redis/example/RedisExampleApplicationTests.java
@@ -14,7 +14,7 @@ public class RedisExampleApplicationTests {
 
     @Autowired
     ICacheProvider jedisCacheProvider;
-    //大三大四999
+    //大三大四999888
 
     @Test
     public void contextLoads() {
